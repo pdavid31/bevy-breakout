25cc01c758928eec145bf8de44cc2e61c279de31
Paul David <paul.david@magenta.de>
2023-12-16 17:48:11 +0100

    Added paddle movement

--- src/paddle.rs
+++ src/paddle.rs
@@ -1,6 +1,9 @@
 use bevy::prelude::*;
 
-use crate::{collision::Collider, wall::BOTTOM_WALL};
+use crate::{
+    collision::Collider,
+    wall::{BOTTOM_WALL, LEFT_WALL, RIGHT_WALL, WALL_THICKNESS},
+};
 
 // These constants are defined in `Transform` units
 // Using the default 2D camera they correspond 1:1 with screen pixels.
@@ -43,6 +46,34 @@ impl PaddleBundle {
     }
 }
 
+fn move_paddle(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<&mut Transform, With<Paddle>>,
+    time: Res<Time>,
+) {
+    let mut paddle_transform = query.single_mut();
+    let mut direction = 0.0;
+
+    if keyboard_input.pressed(KeyCode::Left) {
+        direction -= 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::Right) {
+        direction += 1.0;
+    }
+
+    // calculate the new horizontal paddle position based on player input
+    let new_paddle_position =
+        paddle_transform.translation.x + direction * PADDLE_SPEED * time.delta_seconds();
+
+    // update the paddle position, making sure
+    // it does not cause the paddle to leave the arena
+    let left_bound = LEFT_WALL + WALL_THICKNESS / 2.0 - PADDLE_SIZE.x / 2.0 + PADDLE_PADDING;
+    let right_bound = RIGHT_WALL + WALL_THICKNESS / 2.0 - PADDLE_SIZE.x / 2.0 + PADDLE_PADDING;
+
+    paddle_transform.translation.x = new_paddle_position.clamp(left_bound, right_bound);
+}
+
 fn setup(mut commands: Commands) {
     // calculate the y position of the paddle
     let paddle_y = BOTTOM_WALL + GAP_BETWEEN_PADDLE_AND_FLOOR;
@@ -55,6 +86,7 @@ pub struct PaddlePlugin;
 
 impl Plugin for PaddlePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup);
+        app.add_systems(Startup, setup)
+            .add_systems(FixedUpdate, move_paddle);
     }
 }
