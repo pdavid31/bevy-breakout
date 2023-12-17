cd70fb3f16034f99f36fe72364104af9b76ff952
Paul David <paul.david@magenta.de>
2023-12-17 15:01:56 +0100

    Updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,32 @@ mod background;
 mod ball;
 mod brick;
 mod camera;
-mod collision;
 mod paddle;
 mod score;
-mod velocity;
 mod wall;
 
 use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 use background::BackgroundPlugin;
 use ball::BallPlugin;
 use brick::BrickPlugin;
 use camera::CameraPlugin;
-use collision::CollisionPlugin;
 use paddle::PaddlePlugin;
 use score::ScorePlugin;
-use velocity::VelocityPlugin;
 use wall::WallPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        .add_plugins(RapierDebugRenderPlugin::default())
         .add_plugins(BackgroundPlugin)
         .add_plugins(BallPlugin)
         .add_plugins(BrickPlugin)
         .add_plugins(CameraPlugin)
-        .add_plugins(CollisionPlugin)
         .add_plugins(PaddlePlugin)
         .add_plugins(ScorePlugin)
-        .add_plugins(VelocityPlugin)
         .add_plugins(WallPlugin)
         .run();
 }
